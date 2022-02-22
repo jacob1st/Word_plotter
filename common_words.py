f64fbd2a8f0001d7ae7e83ab77d18dc32336ae32
jacob1st <87782445+jacob1st@users.noreply.github.com>
2022-02-21 19:13:37 -0600

    Update common_words.py

--- common_words.py
+++ common_words.py
@@ -11,12 +11,14 @@ def random_request():
 word_appearances = dict()
 
 iteration = 0
-while iteration < 5:
+total_iterations = 5 # Increase this number to gather information from more articles!
+while iteration < total_iterations:
     soup = random_request()
     # finds the title
     s = soup.find('h1', id='firstHeading')
     print("Article:", s.text)
 
+    # Seperates the words from <p> tags into a dictionary
     lines = soup.findAll('p')
     for line in lines:
         words = str(line.text).split(' ')
@@ -24,7 +26,7 @@ while iteration < 5:
             i = i.lower()
             word_appearances[i] = word_appearances.get(i, 0) + 1
 
-    if iteration < 4:
+    if iteration < total_iterations-1:
         print("Finished. Collecting next article...\n")
     else:
         print("Finished. Gathering final data...\n")
@@ -35,6 +37,7 @@ while iteration < 5:
 top_words = []
 top_values = []
 
+# Sorts the words by value and stores them in a list (By default Order does not matter in dictionaries, it does in lists)
 wordlist=sorted((value, key) for (key,value) in word_appearances.items())
 i = 1
 while i < 6:
@@ -44,11 +47,10 @@ while i < 6:
     i += 1
 
 
+# Plots the bar graph
 fig = plt.figure(figsize = (10, 5))
 plt.bar(top_words, top_values, width=.4, color="maroon")
 plt.ylabel('Occurences')
 plt.xlabel('Words')
 plt.title('Number of word occurences in random wikipedia entries')
 plt.show()
-
-
